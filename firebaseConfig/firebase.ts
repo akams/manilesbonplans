5b6dfe255b78c46ff99a646b443ab9b03fda8cc6
endjawea <Eric.NDJAWEAKAMBIET.ext@eulerhermes.com>
2022-09-17 23:20:58 +0200

    feat: finalize setup work on local firebase

--- firebaseConfig/firebase.ts
+++ firebaseConfig/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,5 +16,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
-
 export const db = getFirestore()
+
+
+if (process.env.NODE_ENV === 'development') {
+  // firebaseApps previously initialized using initializeApp()
+  const db = getFirestore();
+  connectFirestoreEmulator(db, 'localhost', 8082);
+}
+
